8c7a0fd05fbfb9e2a651d3e76be0378e1b669ebb
Alexc <alexsejsi4@gmail.com>
2023-02-04 12:05:19 +0200

    Update styles

--- src/components/TodosList/index.jsx
+++ src/components/TodosList/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { deleteTodo, updateTodo } from '../../store/slices/todosSlice';
+import styles from './TodosList.module.sass';
 
 export const TodosList = ({ todos, remove, update }) => {
   const handleTodoChange = (id, isDone) => update(id, { isDone: !isDone });
 
   return (
-    <ul>
+    <ul className={styles.ul}>
       {todos.map(c => (
         <li key={c.id}>
-          <input
-            type='checkbox'
-            checked={c.isDone}
-            onChange={() => handleTodoChange(c.id, c.isDone)}
-          />
-          {JSON.stringify(c.task)}
+          <div>
+            <input
+              type='checkbox'
+              checked={c.isDone}
+              onChange={() => handleTodoChange(c.id, c.isDone)}
+            />
+            {JSON.stringify(c.task)}
+          </div>
           <button onClick={() => remove(c.id)}>X</button>
         </li>
       ))}
